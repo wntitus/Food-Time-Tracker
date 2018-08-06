aa839ac91ab78da2f0c962101acfe856de79d430
Joseph Venuto <jbvenuto@gmail.com>
2018-08-06 18:16:57 -0400

    Map shows up with the user's location in the center

--- assets/javascript/directions.js
+++ assets/javascript/directions.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
 
     //Global variables
     var userLatLong;
-    var restaurantLocal;
+    var lat;
+    var long;
+    var restaurantLocal = "mcdonalds"
 
     //Function to find user location and store the latitude and longitutde
     function getLocation() {
@@ -10,38 +12,68 @@ $(document).ready(function() {
     };
     function showPosition(position) {
         userLatLong = position.coords.latitude + "," + position.coords.longitude;
-        console.log(userLatLong);
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+        // console.log(userLatLong);
+        console.log("lat: " + lat + " and Long: " + long);
+
+        function initMap(){
+            var mapOption = {
+                center: new google.maps.LatLng(lat, long), 
+                zoom: 15
+            };
+            var map = new google.maps.Map(document.getElementById('map'), mapOption);
+        };
+    
+        initMap ();
+
+
     };
     getLocation();
+    // console.log("lat: " + lat + "and Long: " + long);
+    // var mapurl = "https://maps.googleapis.com/maps/api/staticmap?center=" + userLatLong +"&zoom=14&size=400x300&key=AIzaSyAiP3V7JQ-liMjMuRigFWZCIs3Wc4QR_z8";
+    // $("#map").html("<img src='" + mapurl + "'>");
+
+    // function initMap(){
+    //     var mapOption = {
+    //         center: new google.maps.LatLng(userLatLong), 
+    //         zoom: 10
+    //     };
+    //     var map = new google.maps.Map(document.getElementById('map'), mapOption);
+    // };
+
+    // initMap ();
+
+    
 
     // $(document).on("click", "#go", fucntion() {
     //     restaurantLocal = "temp"
     // });
 
-    function initMap() {
-        var directionsService = new google.maps.DirectionsService();
-        var directionsDisplay = new google.maps.DirectionsRenderer();
-        var userStart = new google.maps.LatLng(userLatLong);
-        var mapOptions = {zoom:7, center:userLatLong};
+    // function initMap() {
+    //     var directionsService = new google.maps.DirectionsService();
+    //     var directionsDisplay = new google.maps.DirectionsRenderer();
+    //     var userStart = new google.maps.LatLng(userLatLong);
+    //     var mapOptions = {zoom:7, center:userLatLong};
 
-        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-        directionsDisplay.setMap(map);
-        directionDisplay.setPanel(document.getElementById('directions'));
-    };
+    //     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    //     directionsDisplay.setMap(map);
+    //     directionDisplay.setPanel(document.getElementById('directions'));
+    // };
 
-    function calcRoute() {
-        var start = userLatLong;
-        var end = "mcdonalds"
-        var request = {
-            origin: start,
-            destination: end,
-            travelMode: 'DRIVING'
-        };
-        directionsService.route(request, function(response, status) {
-            if (status == 'OK') {
-                directionsDisplay.setDirections(response);
-            }
-        });
-    }
+    // function calcRoute() {
+    //     var start = userLatLong;
+    //     var end = "mcdonalds"
+    //     var request = {
+    //         origin: start,
+    //         destination: end,
+    //         travelMode: 'DRIVING'
+    //     };
+    //     directionsService.route(request, function(response, status) {
+    //         if (status == 'OK') {
+    //             directionsDisplay.setDirections(response);
+    //         }
+    //     });
+    // }
 });
 
