441d60290bdefd5a07da978e53fff8fce5727265
wntitus <wesn.titus@gmail.com>
2018-08-06 13:23:36 -0400

    fixed iterator variables on loops

--- APItest/weighting.js
+++ APItest/weighting.js
@@ -1,6 +1,10 @@
 function weighter(){
     navigator.geolocation.getCurrentPosition(location) 
     
+    let namesArray = [];
+    let typesArray = [];
+    let junk = [];
+    let idArray = [];
 
     function location(position) {
         let itemWeight = 0;
@@ -8,49 +12,46 @@ function weighter(){
         let longitude = position.coords.longitude;
 
         const proxy = "https://cors-anywhere.herokuapp.com/"; 
-        const placeURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyAiP3V7JQ-liMjMuRigFWZCIs3Wc4QR_z8&location=" + latitude + "," + longitude + "&radius=8000&keyword=takeaway";
+        const placeURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyAiP3V7JQ-liMjMuRigFWZCIs3Wc4QR_z8&location=" + latitude + "," + longitude + "&radius=8000&keyword=quick,food,takeaway";
        
         
         $.ajax({
             url : proxy + placeURL,
             method : "GET"
-        }).then(function(response) {
-            let namesArray = [];
-            let typesArray = [];
-            let junk = [];
-            let typeCount = 0;
+        }).done(function(response) {
             for (i = 0; i < 20; i++){
                 let output = response.results[i];
-                if (!namesArray.includes(output.name)){
+                if (namesArray.includes(output.name) === false){
                     namesArray.push(output.name);
-                    for (i = 0; i < output.types.length; i++){
-                        typesArray.push(output.types[i]);
+                    idArray.push(output.place_id);
+                    for (j = 0; j < output.types.length; j++){
+                        typesArray.push(output.types[j]);
                     }
                 }
-                let placeID = output.place_id;
-                let detailsURL = "https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyAiP3V7JQ-liMjMuRigFWZCIs3Wc4QR_z8&placeid=" + placeID + "&fields=rating,review";
-
-            $.ajax({
-                url : proxy + detailsURL,
-                method : "GET"
-            }).done(function(response) {
-                console.log(response);
-            })
-            }
-            typesArray.sort();
-            for (i = 0; i < typesArray.length; i++) {
-                let currentElem = typesArray[i];
-                let index = typesArray.indexOf(currentElem);
-                console.log(typeof currentElem);
-                if (currentElem === "point_of_interest") {
-                    typesArray.splice(index);
-                    junk.push(currentElem);
-                } 
-            }
-            console.log(typesArray.length)
+                    typesArray.sort();
+                    for (o = 0; o < typesArray.length; o++) {
+                        let currentElem = typesArray[o];
+                        let index = typesArray.indexOf(currentElem);
+                        if (currentElem === "point_of_interest" || currentElem === "establishment") {
+                            typesArray.splice(index, 1);
+                            junk.push(currentElem);
+                        } 
+                    }
+                }
+                for (k = 0; k < idArray.length; k++) {
+                    let placeID = idArray[k];
+                    let detailsURL = "https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyAiP3V7JQ-liMjMuRigFWZCIs3Wc4QR_z8&placeid=" + placeID + "&fields=rating,review";
+                    $.ajax({
+                        url : proxy + detailsURL,
+                        method : "GET"
+                    }).done(function(response) {
+                        console.log(response);
+                    })
+                    }
             console.log(junk);
             console.log(response);
             console.log(namesArray);
+            console.log(idArray);
             console.log(typesArray);
         })
 
